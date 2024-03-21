a200de8e7a4a1468f05af405493dba186639f5dc
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-21 17:08:55 +0300

    Add working Counter using zustand.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import "./App.css";
+import { useCounterStore } from "./store";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useCounterStore((state) => state.count);
 
   return <OtherComponent count={count} />;
 }
 
 const OtherComponent = (props: { count: number }) => {
-  return <h1>{props.count}</h1>;
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  return (
+    <div>
+      <h1>{props.count}</h1>
+      <div className="buttons">
+        <button className="buttons__button" onClick={decrement}>
+          decrement
+        </button>
+        <button className="buttons__button" onClick={increment}>
+          increment
+        </button>
+      </div>
+    </div>
+  );
 };
 export default App;
