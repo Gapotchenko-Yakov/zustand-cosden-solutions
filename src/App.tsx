d8852a1e146de38d49df150f7bd2385d20c5304f
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-21 16:38:42 +0300

    First commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount] = useState(0);
+
+  return <OtherComponent count={count} />;
 }
 
+const OtherComponent = (props: { count: number }) => {
+  return <h1>{props.count}</h1>;
+};
 export default App;
